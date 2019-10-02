44a0920ea5461172134103725ecaa2fc1af11ad9
Ринат <renich66666622@gmail.com>
2019-10-03 02:30:55 +0300

    0310.1

--- AdvertisementSuperSlayer/AdvertisementSuperSlayer/Games/Snake/SnakePage.xaml.cs
+++ AdvertisementSuperSlayer/AdvertisementSuperSlayer/Games/Snake/SnakePage.xaml.cs
@@ -70,18 +70,18 @@ namespace AdvertisementSuperSlayer.Games.Snake
             points.Add(new Point(10, 90));
             points.Add(new Point(50, 90));
             points.Add(new Point(90, 90));
-            points.Add(new Point(90, 90));
-            points.Add(new Point(90, 90));
-            points.Add(new Point(90, 90));
-            points.Add(new Point(90, 90));
-            points.Add(new Point(90, 90));
+            //points.Add(new Point(90, 90));
+            //points.Add(new Point(90, 90));
+            //points.Add(new Point(90, 90));
+            //points.Add(new Point(90, 90));
+            //points.Add(new Point(90, 90));
 
-
-            Device.StartTimer(TimeSpan.FromMilliseconds(80), ()=>{
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () =>
+            {
                 if (direction == "up")
                 {
                     points.Insert(0, new Point(points[0].X, points[0].Y - 40));
-                    
+
                     points.RemoveAt(points.Count - 1);
                     canvasView.InvalidateSurface();
                     return true;
@@ -116,67 +116,75 @@ namespace AdvertisementSuperSlayer.Games.Snake
             SKSurface surface = e.Surface;
             SKCanvas canvas = surface.Canvas;
             canvas.Clear();
-            
-            SKPaint thick = new SKPaint()
-            {
-                Style = SKPaintStyle.Stroke,
-                Color = SKColors.Purple,
-                StrokeWidth = 10,
-                StrokeCap = SKStrokeCap.Round,
-                IsAntialias = true
-            };
 
-            int maxW = e.Info.Width;
-            int maxH = e.Info.Height;
+            SKPaint kPaint = new SKPaint();
+            kPaint.Color = SKColors.Red;
+            kPaint.StrokeWidth = 5;
+            kPaint.Style = SKPaintStyle.Stroke;
+            SKImageInfo sK = new SKImageInfo(90, 137);
+            open = open.Resize(sK, SKFilterQuality.High);
+            canvas.DrawPoint(100, 100, kPaint);
+            canvas.RotateDegrees(-90, 100, 100);
+            canvas.DrawBitmap(open, 100, 100);
+            //SKPaint thick = new SKPaint()
+            //{
+            //    Style = SKPaintStyle.Stroke,
+            //    Color = SKColors.Purple,
+            //    StrokeWidth = 10,
+            //    StrokeCap = SKStrokeCap.Round,
+            //    IsAntialias = true
+            //};
 
-            SKStrokeJoin strokeJoin = SKStrokeJoin.Round;
-            SKPath path = new SKPath();
-            path.MoveTo(new SKPoint((float)points[points.Count - 1].X, (float)points[points.Count - 1].Y));
+            //int maxW = e.Info.Width;
+            //int maxH = e.Info.Height;
 
-            for (int i = points.Count - 2; i > 0; i--)
-            {
-                path.LineTo(new SKPoint((float)points[i].X, (float)points[i].Y));
-            }
+            //SKStrokeJoin strokeJoin = SKStrokeJoin.Round;
+            //SKPath path = new SKPath();
+            //path.MoveTo(new SKPoint((float)points[points.Count - 1].X, (float)points[points.Count - 1].Y));
 
-            
-            thick.StrokeJoin = strokeJoin;
-            canvas.DrawPath(path, thick);
+            //for (int i = points.Count - 2; i > 0; i--)
+            //{
+            //    path.LineTo(new SKPoint((float)points[i].X, (float)points[i].Y));
+            //}
 
-            SKImageInfo sKImageInfo = new SKImageInfo(125, 190);
-            //open = open.Resize(sKImageInfo, SKFilterQuality.High);
-            if (direction == "up")
-            {
-                canvas.Translate((float)points[0].X, ((float)points[0].Y) );
-                canvas.Scale(0.2f);
-                canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
-                //canvas.Scale(5f);
-                return;
-            }
-            if (direction == "down")
-            {
-                canvas.Translate((float)points[0].X, (float)(points[0].Y));
-                canvas.Scale(0.2f);
-                canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
-                //canvas.Scale(5f);
-                return;
-            }
-            if (direction == "left")
-            {
-                canvas.Translate((float)(points[0].X), (float)points[0].Y);
-                canvas.Scale(0.2f);
-                canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
-                //canvas.Scale(5f);
-                return;
-            }
-            if (direction == "right")
-            {
-                canvas.Translate((float)(points[0].X), (float)points[0].Y);
-                canvas.Scale(0.2f);
-                canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
-                //canvas.Scale(5f);
-                return;
-            }
+
+            //thick.StrokeJoin = strokeJoin;
+            //canvas.DrawPath(path, thick);
+
+            //SKImageInfo sKImageInfo = new SKImageInfo(125, 190);
+            ////open = open.Resize(sKImageInfo, SKFilterQuality.High);
+            //if (direction == "up")
+            //{
+            //    canvas.Translate((float)points[0].X, ((float)points[0].Y) );
+            //    canvas.Scale(0.2f);
+            //    canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
+            //    //canvas.Scale(5f);
+            //    return;
+            //}
+            //if (direction == "down")
+            //{
+            //    canvas.Translate((float)points[0].X, (float)(points[0].Y));
+            //    canvas.Scale(0.2f);
+            //    canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
+            //    //canvas.Scale(5f);
+            //    return;
+            //}
+            //if (direction == "left")
+            //{
+            //    canvas.Translate((float)(points[0].X), (float)points[0].Y);
+            //    canvas.Scale(0.2f);
+            //    canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
+            //    //canvas.Scale(5f);
+            //    return;
+            //}
+            //if (direction == "right")
+            //{
+            //    canvas.Translate((float)(points[0].X), (float)points[0].Y);
+            //    canvas.Scale(0.2f);
+            //    canvas.DrawBitmap(open, e.Info.Rect, BitmapStretch.Uniform);
+            //    //canvas.Scale(5f);
+            //    return;
+            //}
         }
-
     }
 }
