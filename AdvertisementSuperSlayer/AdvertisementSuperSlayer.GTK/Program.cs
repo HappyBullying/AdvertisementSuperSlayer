f388a2f44e7f07ba9fda32404adb5bebbd0f5012
WoWkiller11 <renich66666622@gmail.com>
2019-11-02 23:00:23 +0300

    linuh

---
+++ AdvertisementSuperSlayer/AdvertisementSuperSlayer.GTK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+namespace AdvertisementSuperSlayer.GTK
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+            App app = new App();
+            FormsWindow window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("ASS");
+            window.Show();
+            Gtk.Application.Run();
+        }
+    }
+}
